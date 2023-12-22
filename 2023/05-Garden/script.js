d9cc6662dd1a90ce5a17dd590e62b19adfe882bf
afong <adam.y.w.fong@gmail.com>
2023-12-22 14:48:51 -0800

    Day 5 part 2 brute force attempt

--- 2023/05-Garden/script.js
+++ 2023/05-Garden/script.js
@@ -30,6 +30,37 @@ function minLocation(input) {
   return min
 }
 
+function minLocWithRanges(input) {
+  let min = Number.MAX_VALUE;
+  let [seeds, seedToSoil, soilToFert, fertToWater, waterToLight, lightToTemp, tempToHumid, humidToLoc] = input.split('\r\n\r\n');
+  seeds = seeds.split(': ')[1].split(' ')
+  const maps = [
+    seedToSoil.split(':\r\n')[1].split('\r\n'),
+    soilToFert.split(':\r\n')[1].split('\r\n'),
+    fertToWater.split(':\r\n')[1].split('\r\n'),
+    waterToLight.split(':\r\n')[1].split('\r\n'),
+    lightToTemp.split(':\r\n')[1].split('\r\n'),
+    tempToHumid.split(':\r\n')[1].split('\r\n'),
+    humidToLoc.split(':\r\n')[1].split('\r\n')
+  ]
+  for (let i = 0; i < seeds.length; i += 2) {
+    for (let seed = parseInt(seeds[i]); seed < parseInt(seeds[i + 1]) + parseInt(seeds[i]); seed++) {
+      let temp = seed;
+      for (const map of maps) {
+        for (const rowSTR of map) {
+          const row = rowSTR.split(' ')
+          if ((temp >= parseInt(row[1])) && (temp < (parseInt(row[1]) + parseInt(row[2])))) {
+            temp = temp - parseInt(row[1]) + parseInt(row[0]);
+            break;
+          }
+        }
+      }
+      if (min > temp) min = temp;
+    }
+  }
+  return min
+}
+
 
 
 
@@ -37,6 +68,6 @@ const startTime = performance.now();
 const input = fs.readFileSync(__dirname + '/inputs/input.txt', 'utf8')
 
 const part1 = minLocation(input);
-// // const part2 = totalCards(data)
+const part2 = minLocWithRanges(input);
 const time = performance.now() - startTime;
-console.log(`Part 1: ${part1}\nCompleted in ${time}ms`)
+console.log(`Part 1: ${part1}\nPart 2: ${part2}\nCompleted in ${time}ms`)
